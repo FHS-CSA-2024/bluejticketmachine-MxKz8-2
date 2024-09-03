a70793721f6b040e7987bd73de8791f6d203cc6b
MxKz8-2 <kyletan555@gmail.com>
2024-09-03 08:49:07 -0700

    Update SmarterTicketMachine.java
    My ticket machine, with 3 different priced tickets

--- src/main/java/SmarterTicketMachine.java
+++ src/main/java/SmarterTicketMachine.java
@@ -11,21 +11,25 @@ package src.main.java;
  * @author David J. Barnes and Michael Kölling
  * @version 2016.02.29
  */
-public class SmarterTicketMachine
+public class MyTicketMachine
 {
+    private int seniorPrice;
+    private int childPrice;
     // The price of a ticket from this machine.
-    private int price;
+    private int regularPrice;
     // The amount of money entered by a customer so far.
     private int balance;
     // The total amount of money collected by this machine.
     private int total;
-
+    
     /**
      * Create a machine that issues tickets of the given price.
      */
-    public SmarterTicketMachine(int cost)
+    public MyTicketMachine(int cost)
     {
-        price = cost;
+        regularPrice = cost;
+        seniorPrice = cost/4;
+        childPrice = cost/2;
         balance = 0;
         total = 0;
     }
@@ -33,11 +37,27 @@ public class SmarterTicketMachine
     /**
      * @Return The price of a ticket.
      */
-    public int getPrice()
+    public int getPriceRegular()
     {
-        return price;
+        return regularPrice;
     }
-
+    
+    /**
+     * @Return The price of a ticket.
+     */
+    public int getPriceSenior()
+    {
+        return seniorPrice;
+    }
+    
+    /**
+     * @Return The price of a ticket.
+     */
+    public int getPriceChild()
+    {
+        return childPrice;
+    }
+    
     /**
      * Return The amount of money already inserted for the
      * next ticket.
@@ -67,31 +87,91 @@ public class SmarterTicketMachine
      * reduce the current balance by the ticket price. Print
      * an error message if more money is required.
      */
-    public void printTicket()
+    public void printRegularTicket()
     {
-        boolean checkPrice = (balance >= price);
+        boolean checkPrice = (balance >= regularPrice);
         if(checkPrice) {
             // Simulate the printing of a ticket.
             System.out.println("##################");
             System.out.println("# The BlueJ Line");
             System.out.println("# Ticket");
-            System.out.println("# " + price + " cents.");
+            System.out.println("# " + regularPrice + " cents.");
             System.out.println("##################");
             System.out.println();
 
             // Update the total collected with the price.
-            total = total + price;
+            total = total + regularPrice;
             // Reduce the balance by the price.
-            balance = balance - price;
+            balance = balance - regularPrice;
         }
         else {
-            int amountLeftToPay = price - balance;
+            int amountLeftToPay = regularPrice - balance;
             System.out.println("You must insert at least: " +
                                amountLeftToPay + " more cents.");
                     
         }
     }
 
+        /**
+     * Print a ticket if enough money has been inserted, and
+     * reduce the current balance by the ticket price. Print
+     * an error message if more money is required.
+     */
+    public void printSeniorTicket()
+    {
+        boolean checkPrice = (balance >= seniorPrice);
+        if(checkPrice) {
+            // Simulate the printing of a ticket.
+            System.out.println("##################");
+            System.out.println("# The BlueJ Line");
+            System.out.println("# Ticket");
+            System.out.println("# " + seniorPrice + " cents.");
+            System.out.println("##################");
+            System.out.println();
+
+            // Update the total collected with the price.
+            total = total + seniorPrice;
+            // Reduce the balance by the price.
+            balance = balance - seniorPrice;
+        }
+        else {
+            int amountLeftToPay = seniorPrice - balance;
+            System.out.println("You must insert at least: " +
+                               amountLeftToPay + " more cents.");
+                    
+        }
+    }
+    
+        /**
+     * Print a ticket if enough money has been inserted, and
+     * reduce the current balance by the ticket price. Print
+     * an error message if more money is required.
+     */
+    public void printChildTicket()
+    {
+        boolean checkPrice = (balance >= childPrice);
+        if(checkPrice) {
+            // Simulate the printing of a ticket.
+            System.out.println("##################");
+            System.out.println("# The BlueJ Line");
+            System.out.println("# Ticket");
+            System.out.println("# " + childPrice + " cents.");
+            System.out.println("##################");
+            System.out.println();
+
+            // Update the total collected with the price.
+            total = total + childPrice;
+            // Reduce the balance by the price.
+            balance = balance - childPrice;
+        }
+        else {
+            int amountLeftToPay = childPrice - balance;
+            System.out.println("You must insert at least: " +
+                               amountLeftToPay + " more cents.");
+                    
+        }
+    }
+    
     public int emptyMachine(){
         int originalTotal = total;
         total = 0;
